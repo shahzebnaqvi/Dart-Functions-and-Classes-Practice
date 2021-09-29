3cd4b4222a91aba7df9e744699a4668416655709
shahzebnaqvi <shahzeb.naqvi12@gmail.com>
2021-09-30 01:28:14 +0500

    Question 5,6

---
+++ 6 - sentence using classes print name.dart
@@ -0,0 +1,13 @@
+ void main(){
+  //function call
+    Name object1 = Name();
+    object1.printing();
+}
+// function
+class Name{
+  var firstName ="Syed Muhammad Shahzeb";
+  var lastName = "Naqvi";
+   void printing(){
+  print ("My name is $firstName $lastName");
+   }
+}
