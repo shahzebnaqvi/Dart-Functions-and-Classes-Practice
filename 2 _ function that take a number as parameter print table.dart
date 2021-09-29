1fc834666650f6b0952e30a9e6094c7acf431487
shahzebnaqvi <shahzeb.naqvi12@gmail.com>
2021-09-30 00:35:26 +0500

    Question 2

---
+++ 2 _ function that take a number as parameter print table.dart
@@ -0,0 +1,13 @@
+void main(){
+  //loop  
+    marksheet(8,1,10);
+}
+// function
+void marksheet(  var tableOf, startTab, endTab){
+
+  print ("_____ Table of $tableOf _____");
+  print("----------------------");
+  for (var i=startTab; i<=endTab ; i++){
+    print (".... $tableOf X $i = ${tableOf * i} ....");
+  }
+}
